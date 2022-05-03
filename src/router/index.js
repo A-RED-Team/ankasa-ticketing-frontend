881e551bb3bb6443d1c440d0eacd5d7f328d888d
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-05-03 20:10:22 +0700

    added dashboard admin

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PrivateRoute from '../utils/privateRoute';
+import ScrollToTop from '../utils/scrollToTop';
+
+// Customer
+import Home from '../pages/customer/Home';
+import NotFound from '../pages/customer/NotFound';
+
+// Admin
+import Dashboard from '../pages/admin/Home';
+import NotFoundPage from '../pages/admin/NotFound';
+import Airlines from '../pages/admin/Airlines';
+
+const router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="/*" element={<NotFound />} />
+        </Route>
+        <Route path="admin/" element={<PrivateRoute />}>
+          <Route index element={<Dashboard />} />
+          <Route path="airlines" element={<Airlines />} />
+          <Route path="admin/*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+      <ScrollToTop />
+    </BrowserRouter>
+  );
+};
+export default router;
---
+++ src/utils/customTable.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+$(function () {
+  $('#example1').DataTable({
+    responsive: true
+  });
+});
