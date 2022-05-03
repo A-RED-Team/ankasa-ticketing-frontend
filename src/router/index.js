ce48e44fdf4b0d25206f0261b28de4725486db8b
edoprayoga1999 <edoprayoga.se@gmail.com>
2022-05-03 13:25:22 +0700

    profile page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/main/Home';
 import Login from '../pages/auth/Login';
 import Register from '../pages/auth/Register';
+import Profile from '../pages/main/Profile';
 import ScrollToTop from '../utils/ScrollToTop';
 
 const router = () => {
@@ -17,6 +18,9 @@ const router = () => {
         </Route>
         <Route path="/register">
           <Route index element={<Register />} />
+        </Route>
+        <Route path="/profile">
+          <Route index element={<Profile />} />
         </Route>
       </Routes>
       <ScrollToTop />
