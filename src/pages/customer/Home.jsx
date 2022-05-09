cc0d219467c4b0a148457328b8fd5093a11d49fc
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-05-10 06:55:49 +0700

    fix: trending city

--- src/pages/customer/Home.jsx
+++ src/pages/customer/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Navbar from '../../components/customer/Navbar';
 import Hero from '../../components/customer/Hero';
@@ -6,12 +6,32 @@ import Card from '../../components/customer/Card';
 import Carousel from '../../components/customer/Carousel';
 import Footer from '../../components/customer/Footer';
 
+import ContentLoader from 'react-content-loader';
+import { useSelector, useDispatch } from 'react-redux';
+import { getTrendingCity } from '../../redux/actions/trending';
+
 const Home = () => {
+  const dispatch = useDispatch();
+  const trending = useSelector((state) => state.trending);
+
+  useEffect(() => {
+    dispatch(getTrendingCity('cities.created_at', 'ASC', 5));
+  }, []);
+
   return (
     <>
       <Navbar />
       <Hero />
-      <Card />
+      {trending.isLoading ? (
+        // untuk membuat loading page
+        <ContentLoader />
+      ) : trending.isError ? (
+        // Disini bisa memasukkan sweet alert untuk error
+        <div>Error</div>
+      ) : (
+        // Mengirim data trending ke komponent card
+        <Card trending={trending} />
+      )}
       <Carousel />
       <Footer />
     </>
