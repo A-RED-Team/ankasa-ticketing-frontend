dc8c0f3f2ce415f2e59bfedc0e0ffd12c16900e8
Nur Muhammad Alif Putra Setiawan <39686865+altrawan@users.noreply.github.com>
2022-05-10 21:51:42 +0700

    Update Profile.jsx

--- src/pages/customer/Profile.jsx
+++ src/pages/customer/Profile.jsx
@@ -156,7 +156,6 @@ const Profile = () => {
                     backgroundColor: '#FFF',
                     border: 'none',
                     borderRadius: '100px',
-//                     backgroundImage: `url(${process.env.REACT_APP_PROD}uploads/users/${detailUser.data.data.photo})`,
                     backgroundRepeat: 'no-repeat',
                     backgroundSize: 'cover'
                   }}></div>
