3c6eb2aadf1b73a1bba5a37598fc16ad473a1f98
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-05-12 06:51:19 +0700

    fix: responsive auth

--- src/assets/styles/AuthStyles.jsx
+++
@@ -1,7 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-
-const AuthStyles = createGlobalStyle`
-  
-`;
-
-export default AuthStyles;
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,125 +1,28 @@
-import React from 'react';
-import styled from 'styled-components';
-// import Modal from '../Search';
+import React, { useState } from 'react';
+import Modal from '../Search';
 import { Link, useNavigate } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import { APP_STAGING, APP_DEV, APP_PROD } from '../../helper/env';
-
 import '../../utils/navbar.js';
-import './navbar.css';
 
 import logo from '../../assets/icons/illustration.svg';
 import toggle from '../../assets/icons/align-right.svg';
 import search from '../../assets/icons/search.svg';
 import mail from '../../assets/icons/ic_round-mail-outline.svg';
 import bell from '../../assets/icons/bell.svg';
-import profile from '../../assets/icons/user.png';
-import exit from '../../assets/icons/log-out.png';
-
-const Search = styled.div`
-  border-radius: 10px;
-  width: 250px;
-  display: flex;
-  align-items: center;
-  background-color: #efefef;
-  margin-right: 20px;
-
-  input {
-    font-family: 'Lato', 'sans-serif';
-    font-weight: 400;
-    font-size: 14px;
-    margin-left: 10px;
-  }
-
-  @media screen and (max-width: 576px) {
-    width: 100%;
-    margin: 10px 0;
-  }
-`;
-
-const Icon = styled.label`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-
-  img {
-    position: absolute;
-    top: 0;
-    bottom: -5px;
-    margin: auto;
-  }
-`;
-
-const Button = styled.button`
-  background-color: var(--text-blue);
-  color: var(--text-white);
-  padding: 11px 13px;
-  border: none;
-  border-radius: 7px;
-  font-size: 16px;
-  font-weight: 700;
-  font-family: 'Poppins', sans-serif;
-  text-transform: capitalize;
-  transition: all 0.5 ease;
-  font-weight: 500;
-  text-decoration: none;
-  width: 130px;
-  text-align: center;
-
-  &:hover {
-    box-shadow: 0px 8px 10px rgba(35, 149, 255, 0.3);
-  }
-
-  @media screen and (max-width: 576px) {
-    display: block;
-    margin: 0 auto;
-    margin-top: 10px;
-  }
-`;
-
-const Profile = styled.div`
-  display: flex;
-  width: 200px;
-  height: 27px;
-  justify-content: space-between;
-`;
-
-// const Photo = styled.img`
-//   width: 45px;
-//   height: 45px;
-//   margin-top: -10px;
-//   border: 2px solid #2395ff;
-//   border-radius: 25px;
-//   overflow: hidden;
-//   padding: 3px;
-// `;
 
 const index = ({ isLogin = false }) => {
   const navigate = useNavigate();
-  // const [modal, setModal] = useState(false);
-  // const [isLogin, setIsLogin] = useState(false);
-  // const token = localStorage.getItem('token');
-  // let decoded = '';
+  const [modal, setModal] = useState(false);
 
   let decoded = '';
   if (isLogin) {
     decoded = jwt_decode(isLogin);
   }
 
-  const menuToggle = () => {
-    const toggleMenu = document.querySelector('.menu');
-    toggleMenu.classList.toggle('active');
-  };
-
-  const logout = () => {
-    localStorage.clear();
-    navigate('/login');
-  };
-
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-white">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-white">
         <div className="container">
           <img src={logo} alt="Ankasa Ticketing" className="mr-2" />
           <a className="navbar-brand" href="/">
@@ -138,10 +41,10 @@ const index = ({ isLogin = false }) => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav m-auto">
               <form>
-                <Search className="p-1 pl-4">
-                  <Icon>
-                    <img src={search} alt="Search" />
-                  </Icon>
+                <div className="navbar-search p-1 pl-4">
+                  <label htmlFor="search" className="navbar-icon-search">
+                    <img src={search} alt="Search" id="search" />
+                  </label>
                   <input
                     type="search"
                     className="form-control"
@@ -153,27 +56,29 @@ const index = ({ isLogin = false }) => {
                     }}
                     placeholder="Where you want to go ?"
                   />
-                </Search>
+                </div>
               </form>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a
+                  onClick={() => setModal(!modal)}
+                  className={modal ? 'nav-link active' : 'nav-link'}>
                   find ticket
                 </a>
               </li>
               {isLogin && (
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <Link to="/profile/booking" className="nav-link">
                     my booking
-                  </a>
+                  </Link>
                 </li>
               )}
             </ul>
             {isLogin ? (
-              <Profile>
+              <div className="navbar-profile">
                 <img src={mail} alt="Message" />
                 <img src={bell} alt="Notification" />
-                <div className="action">
-                  <div className="profile" onClick={menuToggle}>
+                <div className="navbar-photo">
+                  <Link to="/profile">
                     <img
                       src={`${
                         APP_STAGING === 'dev'
@@ -182,104 +87,18 @@ const index = ({ isLogin = false }) => {
                       }`}
                       alt={decoded.name}
                     />
-                  </div>
-                  <div className="menu">
-                    <h3>{decoded.username}</h3>
-                    <ul>
-                      <li>
-                        <img src={profile} alt="My Profile" />
-                        <Link to="/profile">My Profile</Link>
-                      </li>
-                      <li>
-                        <img src={exit} alt="Logout" />
-                        <a onClick={logout}>Logout</a>
-                      </li>
-                    </ul>
-                  </div>
+                  </Link>
                 </div>
-              </Profile>
+              </div>
             ) : (
-              <Button onClick={() => navigate('/register')}>Sign Up</Button>
+              <button className="navbar-button" onClick={() => navigate('/register')}>
+                Sign Up
+              </button>
             )}
           </div>
         </div>
       </nav>
-      {/* <Navbar color="white" expand="lg" className="fixed-top" light>
-        <Container>
-          <img src={logo} alt="Ankasa Ticketing" className="mr-2" />
-          <NavbarBrand href="/">ankasa</NavbarBrand>
-          <NavbarToggler onClick={() => setIsOpen(!isOpen)}>
-            <img src={toggle} alt="toggle" />
-          </NavbarToggler>
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="m-auto" navbar>
-              <form>
-                <Search className="p-1 pl-4">
-                  <Icon>
-                    <img src={search} alt="Search" />
-                  </Icon>
-                  <input
-                    type="search"
-                    className="form-control"
-                    style={{
-                      backgroundColor: 'transparent',
-                      border: 'none',
-                      outline: 'none',
-                      boxShadow: 'none'
-                    }}
-                    placeholder="Where you want to go ?"
-                  />
-                </Search>
-              </form>
-              <NavItem>
-                <NavLink onClick={() => setModal(!modal)}>find ticket</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  tag={Link}
-                  to="/profile/booking"
-                  active={location.pathname === '/profile/booking'}>
-                  my booking
-                </NavLink>
-              </NavItem>
-            </Nav>
-            {isLogin ? (
-              <Profile>
-                <img src={mail} alt="Message" />
-                <img src={bell} alt="Notification" />
-                <div className="action">
-                  <div className="profile" onClick={menuToggle}>
-                    <img
-                      src={`${
-                        APP_STAGING === 'dev'
-                          ? `${APP_DEV}uploads/users/${decoded.photo}`
-                          : `${APP_PROD}uploads/users/${decoded.photo}`
-                      }`}
-                      alt={decoded.name}
-                    />
-                  </div>
-                  <div className="menu">
-                    <h3>{decoded.username}</h3>
-                    <ul>
-                      <li>
-                        <img src={profile} alt="My Profile" />
-                        <Link to="/profile">My Profile</Link>
-                      </li>
-                      <li>
-                        <img src={exit} alt="Logout" />
-                        <a onClick={logout}>Logout</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </Profile>
-            ) : (
-              <Button onClick={() => navigate('/register')}>Sign Up</Button>
-            )}
-          </Collapse>
-        </Container>
-      </Navbar> */}
-      {/* {modal ? <Modal /> : null} */}
+      {modal ? <Modal /> : null}
     </>
   );
 };
